add0d29bf61ca0197670c01db181e02011342186
Mateus Cesar <mateuscesarglima@gmail.com>
2022-07-14 14:27:07 -0300

    feat(CHALLENGE-6): implemented subscription, status, eventstory entites and respectives dtos

--- src/main/java/com/academy/challenge/dto/UserDto.java
+++ src/main/java/com/academy/challenge/dto/UserDto.java
@@ -1,12 +1,15 @@
 package com.academy.challenge.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserDto {
-  
+
+  @NotBlank
   private String name;
 
 }
---
+++ src/main/java/com/academy/challenge/entities/EventStory.java
@@ -0,0 +1,36 @@
+package com.academy.challenge.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_event_history")
+public class EventStory {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "subscription_id")
+  private Subscription subscription;
+
+  private String type;
+
+  private LocalDateTime created_at;
+
+}

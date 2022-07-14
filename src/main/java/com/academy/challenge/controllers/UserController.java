165e558ae55338f0f52604630f138c270cf01954
Mateus Cesar <mateuscesarglima@gmail.com>
2022-07-14 14:59:11 -0300

    feat(CHALLENGE-7) added findById endpoint and getAll in userController

--- src/main/java/com/academy/challenge/controllers/UserController.java
+++ src/main/java/com/academy/challenge/controllers/UserController.java
@@ -2,12 +2,18 @@ package com.academy.challenge.controllers;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +31,24 @@ public class UserController {
   private UserServices UserServices;
 
   @PostMapping
-  public ResponseEntity<Object> createUser(@RequestBody UserDto userDto) {
+  public ResponseEntity<Object> createUser(@RequestBody @Valid UserDto userDto) {
     var user = new User();
     BeanUtils.copyProperties(userDto, user);
     user.setCreated_at(LocalDateTime.now(ZoneId.of("UTC")));
     return ResponseEntity.status(HttpStatus.CREATED).body(UserServices.saveUser(user));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<User>> findAll() {
+    return ResponseEntity.status(HttpStatus.OK).body(UserServices.findAll());
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<Object> findById(@PathVariable(value = "id") UUID id) {
+    Optional<User> user = UserServices.findById(id);
+    if (!user.isPresent()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(user.get());
   }
 }

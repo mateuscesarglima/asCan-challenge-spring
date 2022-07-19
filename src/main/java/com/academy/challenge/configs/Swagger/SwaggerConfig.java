869609a8c531f1f10146daf5436a53628c95c8b7
Mateus Cesar <mateuscesarglima@gmail.com>
2022-07-19 12:41:18 -0300

    feat(CHALLENGE-17): implemented swagger configuration and improved project documentation on readme.md

---
+++ src/main/java/com/academy/challenge/configs/Swagger/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.academy.challenge.configs.Swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public Docket getBean() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.regex("/api.*"))
+        .build()
+        .apiInfo(getInfo());
+  }
+
+  private ApiInfo getInfo() {
+    return new ApiInfoBuilder().title("Academy Challenge API").description("Api Guidelines ").license("1.0.0").build();
+  }
+
+}
